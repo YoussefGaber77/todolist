b1e935fa00d6ab3bf4295bde7e774bf97958d49a
Hania Abdalla <64498492+haniaabdalla@users.noreply.github.com>
2022-01-05 01:32:25 +0200

    Update databaseConnection.js

--- server/databaseConnection.js
+++ server/databaseConnection.js
@@ -3,7 +3,7 @@ const DataBaseconnection = async() => {
 
 
     await mongoose.connect(
-        "mongodb://"+process.env.IP+"27017/todos", {
+        "mongodb://"+process.env.IP+":27017/todos", {
             useNewUrlParser: true,
             useCreateIndex: true,
             useUnifiedTopology: true,
